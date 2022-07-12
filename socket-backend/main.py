cae8d4228bd061b4d743a25164451c9d52e6631a
Andrei Chenchik <andrei@chenchik.me>
2022-07-12 11:11:03 +0200

    update names

--- socket-backend/main.py
+++ socket-backend/main.py
@@ -1,47 +1,25 @@
-from typing import List
+from typing import List, Optional
 
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
+from pydantic import BaseSettings
+
+from html import html
 
 app = FastAPI()
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <h2>Your ID: <span id="ws-id"></span></h2>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var client_id = Date.now()
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
+class Settings(BaseSettings):
+    host: str
+    port: Optional[str] = None
 
+    class Config:
+        env_prefix = "WS_"
+
+settings = Settings()
+location = settings.host
+
+if settings.port:
+    location += f":{settings.port}"
 
 class ConnectionManager:
     def __init__(self):
@@ -64,17 +42,17 @@ manager = ConnectionManager()
 
 @app.get("/")
 async def get():
-    return HTMLResponse(html)
+    return HTMLResponse(html(location))
 
 
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
+@app.websocket("/ws/{client_name}")
+async def websocket_endpoint(websocket: WebSocket, client_name: str):
     await manager.connect(websocket)
-    await manager.broadcast(f"Client #{client_id} enters the chat")
+    await manager.broadcast(f"{client_name} enters the chat")
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            await manager.broadcast(f"{client_name}: {data}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+        await manager.broadcast(f"{client_name} left the chat")
