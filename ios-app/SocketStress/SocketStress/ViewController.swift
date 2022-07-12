e80513b81873a00969352b6f2475d840739d1165
Andrei Chenchik <andrei@chenchik.me>
2022-07-12 14:19:52 +0200

    add app

---
+++ ios-app/SocketStress/SocketStress/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocketStress
+//
+//  Created by Andrei Chenchik on 12/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemCyan
+    }
+
+
+}
+
